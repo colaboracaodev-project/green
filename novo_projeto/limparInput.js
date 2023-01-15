e5e171b23590a5adea807be808a1599746fb3a10
viniciusoliver-stack <vinicius.so.contato@hotmail.com>
2023-01-15 09:43:21 -0300

    Correção na construção do código

--- novo_projeto/limparInput.js
+++ novo_projeto/limparInput.js
@@ -1,11 +1,6 @@
-function calculaTotal(saldoSalario, feriasVencidas, feriasProporcionais, decimoTerceiroProporcional, avisoPrevioIdenizado, multaRescisoria) {
-  let total = saldoSalario + feriasVencidas + feriasProporcionais + decimoTerceiroProporcional + avisoPrevioIdenizado + multaRescisoria;
-
+function limparInput(){
   document.getElementById("salario").value = ''
   document.getElementById("dias-trabalhados").value = ''
   document.getElementById("meses-trabalhados-ano").value = ''
   document.getElementById("total-de-meses-trabalhados").value = ''
-  
-  console.log(typeof(total));
-  return total.toFixed(2);  
 }
