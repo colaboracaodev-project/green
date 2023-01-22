b28326269d666db9e226271cd7270a5f9a6a7b8c
Eduardo Escudero <escudero.ti@gmail.com>
2023-01-22 17:15:17 -0300

    att

--- novo_projeto/limparInput.js
+++
@@ -1,6 +0,0 @@
-function limparInput(){
-  document.getElementById("salario").value = ''
-  document.getElementById("dias-trabalhados").value = ''
-  document.getElementById("meses-trabalhados-ano").value = ''
-  document.getElementById("total-de-meses-trabalhados").value = ''
-}
